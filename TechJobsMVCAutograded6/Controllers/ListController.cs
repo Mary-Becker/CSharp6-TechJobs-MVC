eb1ab1b3dfa1bc0b4f9a64647c298fc35b73c06f
JC <marybecker053@gmail.com>
2024-06-21 17:32:10 -0500

    Task 2 Done

--- TechJobsMVCAutograded6/Controllers/ListController.cs
+++ TechJobsMVCAutograded6/Controllers/ListController.cs
@@ -44,6 +44,19 @@ public class ListController : Controller
     // TODO #2 - Complete the Jobs action method
     public IActionResult Jobs(string column, string value)
     {
+        List<Job> jobs;
+        if (column.ToLower() == "all")
+        {
+            jobs = JobData.FindAll();
+            ViewBag.title = "All Jobs";
+        }
+        else
+        {
+            jobs = JobData.FindByColumnAndValue(column, value);
+            ViewBag.title = $"Jobs with {ColumnChoices[column]}: {value}";
+        }
+
+        ViewBag.jobs = jobs;
         return View();
     }
 }
