bb821874b3ac141f156f5d22db5a80ba0b7a1d5d
JC <marybecker053@gmail.com>
2024-06-27 15:00:42 -0500

    GA 3 DONE

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -6,17 +6,37 @@ using Microsoft.AspNetCore.Mvc;
 using TechJobsMVCAutograded6.Data;
 using TechJobsMVCAutograded6.Models;
 
-namespace TechJobsMVCAutograded6.Controllers;
-
-public class SearchController : Controller
+namespace TechJobsMVCAutograded6.Controllers
 {
-    // GET: /<controller>/
-    public IActionResult Index()
+    public class SearchController : Controller
     {
+    // GET: /<controller>/
+        public IActionResult Index()
+        {
         ViewBag.columns = ListController.ColumnChoices;
         return View();
-    }
+        }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
-}
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> jobs;
+
+            if (searchTerm ==  null || (searchType == "all" && searchTerm == ""))
+            {
+                jobs = JobData.FindAll();
+            }  
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            ViewBag.jobs = jobs;
+            ViewBag.columns = ListController.ColumnChoices;
+
+
+            return View("Index");
+        }
+    }
+}
